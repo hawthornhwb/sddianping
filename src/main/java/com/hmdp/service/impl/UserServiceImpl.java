3dcee6d81bb401db423b80d614481bacf813b2bc
hawthornhwb <15715728255@163.com>
2025-10-09 14:04:27 +0800

    完成短信验证码的登录与注册

--- src/main/java/com/hmdp/service/impl/UserServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -2,6 +2,7 @@ package com.hmdp.service.impl;
 
 import cn.hutool.core.util.RandomUtil;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.dto.LoginFormDTO;
 import com.hmdp.dto.Result;
 import com.hmdp.entity.User;
 import com.hmdp.mapper.UserMapper;
@@ -11,6 +12,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpSession;
+
+import static com.hmdp.utils.SystemConstants.USER_NICK_NAME_PREFIX;
 
 /**
  * <p>
@@ -41,5 +44,41 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
         log.info("打印验证码:{}", code);
         // 5. 返回正确信息
         return Result.ok();
+    }
+
+    @Override
+    public Result login(LoginFormDTO loginForm, HttpSession session) {
+        // 1. 校验手机号 注：每次请求都是独立的请求，因而需要再次校验手机号
+        String phone = loginForm.getPhone();
+        if (RegexUtils.isPhoneInvalid(phone)) {
+            // 1.1 手机号不符合，则返回错误信息
+            return Result.fail("login: 手机号格式错误");
+        }
+        // 2. 校验验证码
+        String code = loginForm.getCode();
+        if (code == null || !session.getAttribute("code").toString().equals(code)) {
+            return Result.fail("login: 验证码错误或不存在");
+        }
+        // 3. 根据手机号查询用户
+        User user = query().eq("phone", phone).one();
+        // 4. 用户不存在
+        if (user == null) {
+            // 4.1 创建新用户，将用户信息保存到数据库中
+            user = createUserWithPhone(phone);
+        }
+
+        // 5. 将用户信息保存到session中
+        session.setAttribute("user", user);
+
+        return Result.ok(); // 基于session不需要返回登录凭证
+    }
+
+    private User createUserWithPhone(String phone) {
+        // 4.1 创建新用户，将用户信息保存到数据库中
+        User user = new User();
+        user.setPhone(phone);
+        user.setNickName(USER_NICK_NAME_PREFIX + RandomUtil.randomString(10));
+        save(user);
+        return user;
     }
 }
